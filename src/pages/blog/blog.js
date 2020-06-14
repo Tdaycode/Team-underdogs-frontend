429ab127431163b26a7a363b00b63dee8dde9ff2
yasholma <yasholma1@gmail.com>
2020-06-14 20:04:34 +0100

    merge develop

--- src/pages/blog/blog.js
+++ src/pages/blog/blog.js
@@ -2,100 +2,135 @@ import React, { Fragment } from 'react'
 import './blog.css'
 import { Link } from 'react-router-dom'
 
-
-
-import Laptop from "../../assets/images/Blog/laptop guy.svg"
-import Gift from "../../assets/images/Blog/gift guys.svg"
-import Ethereum from "../../assets/images/Blog/ethereum.svg"
-import Merry from "../../assets/images/Blog/merry.svg"
-
-
+import Laptop from '../../assets/images/Blog/laptop guy.svg'
+import Gift from '../../assets/images/Blog/gift guys.svg'
+import Ethereum from '../../assets/images/Blog/ethereum.svg'
+import Merry from '../../assets/images/Blog/merry.svg'
 
 function Blog() {
   return (
     <Fragment>
-      <section className="background-img">
-          <div className="container">
-          <div className="row">
-            <div className="col-sm-12 col-md-4 col-lg-4 mx-auto mt-5">
-              <div className="left-text">
+      <section className='background-img'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-sm-12 col-md-4 col-lg-4 mx-auto mt-5'>
+              <div className='left-text'>
                 <h1>
-                  Stay updated,<br />
+                  Stay updated,
+                  <br />
                   Never miss a post.
                 </h1>
                 <p>
-                  Get to know the latest stuff about our services<br />
+                  Get to know the latest stuff about our services
+                  <br />
                   via our blog post.
                 </p>
               </div>
             </div>
 
-            <div className="col-sm-12 col-md-8 col-lg-8 mt-4">
-              <div className="laptop">
-                <img className="img-fluid" src={Laptop} alt="laptop guy" />
+            <div className='col-sm-12 col-md-8 col-lg-8 mt-4'>
+              <div className='laptop'>
+                <img className='img-fluid' src={Laptop} alt='laptop guy' />
               </div>
             </div>
           </div>
         </div>
       </section>
 
-      <section className="gift-section">
-        <div className="container">
-          <div className="row">
-            <div class="card ml-3 mr-3 mt-5">
-            <img src={Gift} class="card-img-top" alt="" />
-            <div class="card-body">
-              <h5 class="card-title">HOW TO SELL YOUR ITUNES GIFTCARD WITH US.</h5>
-              <p class="card-text text-center">19 JUNE, 2019 | BUSINESS</p>
-              <p class="card-text text-center">Lorem ipsum dolor sit amet, magna habemus ius ad, qui minimum voluptaria in. Ad mei modus quodsi</p>
-              <p class="card-text text-center">complectitur, postea verterem persecuti cu est, sea epicuri.</p>
-              <Link to="/blog/ethereum" class="btn mt-3 mb-3">DISCOVER</Link>
+      <section className='gift-section'>
+        <div className='container'>
+          <div className='row'>
+            <div className='card ml-3 mr-3 mt-5'>
+              <img src={Gift} className='card-img-top' alt='' />
+              <div className='card-body'>
+                <h5 className='card-title'>
+                  HOW TO SELL YOUR ITUNES GIFTCARD WITH US.
+                </h5>
+                <p className='card-text text-center'>
+                  19 JUNE, 2019 | BUSINESS
+                </p>
+                <p className='card-text text-center'>
+                  Lorem ipsum dolor sit amet, magna habemus ius ad, qui minimum
+                  voluptaria in. Ad mei modus quodsi
+                </p>
+                <p className='card-text text-center'>
+                  complectitur, postea verterem persecuti cu est, sea epicuri.
+                </p>
+                <Link to='/blog/ethereum' className='btn mt-3 mb-3'>
+                  DISCOVER
+                </Link>
+              </div>
             </div>
-          </div>
           </div>
         </div>
       </section>
 
-      <section className="merry-section">
-        <div className="container">
-          <div className="row">
-            <div class="card ml-3 mr-3 mt-5">
-            <img src={Merry} class="card-img-top" alt="" />
-            <div class="card-body">
-              <h5 class="card-title">HOW TO SELL YOUR ITUNES GIFTCARD WITH US.</h5>
-              <p class="card-text text-center">19 JUNE, 2019 | BUSINESS</p>
-              <p class="card-text text-center">Lorem ipsum dolor sit amet, magna habemus ius ad, qui minimum voluptaria in. Ad mei modus quodsi</p>
-              <p class="card-text text-center">complectitur, postea verterem persecuti cu est, sea epicuri.</p>
-              <Link to="/blog/ethereum" class="btn mt-3 mb-3">DISCOVER</Link>
+      <section className='merry-section'>
+        <div className='container'>
+          <div className='row'>
+            <div classNames='card ml-3 mr-3 mt-5'>
+              <img src={Merry} classNames='card-img-top' alt='' />
+              <div classNames='card-body'>
+                <h5 classNames='card-title'>
+                  HOW TO SELL YOUR ITUNES GIFTCARD WITH US.
+                </h5>
+                <p classNames='card-text text-center'>
+                  19 JUNE, 2019 | BUSINESS
+                </p>
+                <p classNames='card-text text-center'>
+                  Lorem ipsum dolor sit amet, magna habemus ius ad, qui minimum
+                  voluptaria in. Ad mei modus quodsi
+                </p>
+                <p classNames='card-text text-center'>
+                  complectitur, postea verterem persecuti cu est, sea epicuri.
+                </p>
+                <Link to='/blog/ethereum' classNames='btn mt-3 mb-3'>
+                  DISCOVER
+                </Link>
+              </div>
             </div>
-          </div>
           </div>
         </div>
       </section>
 
-      <section className="Ethereum-section">
-        <div className="container">
-          <div className="row">
-            <div class="card ml-3 mr-3 mt-5">
-            <img src={Ethereum} class="card-img-top" alt="" />
-            <div class="card-body">
-              <h5 class="card-title">HOW TO SELL YOUR ITUNES GIFTCARD WITH US.</h5>
-              <p class="card-text text-center">19 JUNE, 2019 | BUSINESS</p>
-              <p class="card-text text-center">Lorem ipsum dolor sit amet, magna habemus ius ad, qui minimum voluptaria in. Ad mei modus quodsi</p>
-              <p class="card-text text-center">complectitur, postea verterem persecuti cu est, sea epicuri.</p>
-              <Link to="/blog/ethereum" class="btn mt-3 mb-3">DISCOVER</Link>
+      <section className='Ethereum-section'>
+        <div className='container'>
+          <div className='row'>
+            <div className='card ml-3 mr-3 mt-5'>
+              <img src={Ethereum} className='card-img-top' alt='' />
+              <div className='card-body'>
+                <h5 className='card-title'>
+                  HOW TO SELL YOUR ITUNES GIFTCARD WITH US.
+                </h5>
+                <p className='card-text text-center'>
+                  19 JUNE, 2019 | BUSINESS
+                </p>
+                <p className='card-text text-center'>
+                  Lorem ipsum dolor sit amet, magna habemus ius ad, qui minimum
+                  voluptaria in. Ad mei modus quodsi
+                </p>
+                <p className='card-text text-center'>
+                  complectitur, postea verterem persecuti cu est, sea epicuri.
+                </p>
+                <Link to='/blog/ethereum' className='btn mt-3 mb-3'>
+                  DISCOVER
+                </Link>
+              </div>
             </div>
-          </div>
           </div>
         </div>
       </section>
 
-      <section className="pagination-section">
-        <div className="circles" id="onez">01</div>
-        <div className="circles">02</div>
-        <div className="circles">03</div>
-        <div className="circles">...</div>
-        <div className="circles" id="button">Next</div>
+      <section className='pagination-section'>
+        <div className='circles' id='onez'>
+          01
+        </div>
+        <div className='circles'>02</div>
+        <div className='circles'>03</div>
+        <div className='circles'>...</div>
+        <div className='circles' id='button'>
+          Next
+        </div>
       </section>
     </Fragment>
   )
